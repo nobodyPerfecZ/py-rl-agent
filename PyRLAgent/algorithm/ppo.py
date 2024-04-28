D. J.: - Added unittest for creating a transition with batch settings - Fixed the computation of GAE

PyRLAgent/algorithm/ppo.py
@@ -234,24 +234,24 @@ class PPO(Algorithm):
 
         Args:
             rewards (torch.Tensor):
-                Minibatch of rewards with shape (NUM_ENVS, NUM_STEPS)
+                Minibatch of rewards with shape (NUM_STEPS, NUM_ENVS)
 
             dones (torch.Tensor):
-                Minibatch of dones with shape (NUM_ENVS, NUM_STEPS)
+                Minibatch of dones with shape (NUM_STEPS, NUM_ENVS)
 
             values (torch.Tensor):
-                Minibatch of values V(s) with shape (NUM_ENVS, NUM_STEPS)
+                Minibatch of values V(s) with shape (NUM_STEPS, NUM_ENVS)
 
             next_values (torch.Tensor):
-                Minibatch of next values V(s_t+1) with shape (NUM_ENVS, NUM_STEPS)
+                Minibatch of next values V(s_t+1) with shape (NUM_STEPS, NUM_ENVS)
 
         Returns:
             tuple[torch.Tensor, torch.Tensor]:
                 advantages (torch.Tensor):
-                    Minibatch of computed advantages with shape (NUM_ENVS, NUM_STEPS)
+                    Minibatch of computed advantages with shape (NUM_STEPS, NUM_ENVS)
 
                 targets (torch.Tensor):
-                    Minibatch of computed targets with shape (NUM_ENVS, NUM_STEPS)
+                    Minibatch of computed targets with shape (NUM_STEPS, NUM_ENVS)
         """
         # Compute temporal difference errors (deltas)
         # delta_t = r_t + gamma * (1-dones) * V(s_t+1) - V(s_t)
@@ -260,9 +260,9 @@ class PPO(Algorithm):
         # Compute advantages using Generalized Advantage Estimation (GAE)
         # A_t = delta_t + gamma * gae_lambda * (1-dones) * A_t+1
         advantages = torch.zeros(rewards.shape)
-        advantages[:, -1] = deltas[:, -1]
+        advantages[-1, :] = deltas[-1, :]
         for t in reversed(range(self.steps_per_trajectory - 1)):
-            advantages[:, t] = deltas[:, t] + self.gamma * self.gae_lambda * ~dones[:, t] * advantages[:, t + 1]
+            advantages[t, :] = deltas[t, :] + self.gamma * self.gae_lambda * ~dones[t, :] * advantages[t + 1, :]
 
         # Normalization of the advantages
         advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)

PyRLAgent/common/buffer/abstract_buffer.py
@@ -9,45 +9,75 @@ class Transition(NamedTuple):
     """
     A class for representing a Transition in a reinforcement learning environment.
 
-    A Transition (s, a, r, s', done, log_prob) is used to capture the key elements of an agent interaction with an
-    environment:
-        - s := current state
-        - a := taken action
-        - r := reward for doing action a from state s
-        - s' := next state after taking action a from state s
-        - done := Is state s' a terminal state or not
-        - log_prob := log probability of p(a | s)
-        - value := state-value V(s)
+    A Transition (s_t, a_t, r_t, s_t+1, done, log_prob, value) is used to capture the key elements of an agent
+    interaction with an environment:
+        - s_t := current state
+        - a_t := taken action
+        - r_t := reward for doing action a_t from state s_t
+        - s_t+1 := next state after taking action a_t from state s_t
+        - done := Is state s_t+1 a terminal state or not
+        - (Optional:) log_prob := log probability of p(a_t | s_t)
+        - (Optional:) value := state-value function V(s_t)
 
     Attributes:
-        state (Union[np.ndarray, torch.Tensor]):
-            The current state s
+        state (np.ndarray | torch.Tensor):
+            The current state s_t in time step t
 
-        action (Union[np.ndarray, torch.Tensor]):
-            The taken action a
+        action (np.ndarray | torch.Tensor):
+            The taken action a_t in time step t
 
-        reward (Union[np.ndarray, torch.Tensor]):
-            The reward r for doing action a from state s
+        reward (np.ndarray | torch.Tensor):
+            The reward r_t for doing action a_t from state s_t
 
-        next_state (Union[np.ndarray, torch.Tensor]):
-            The next state s' after taking action a from state s
+        next_state (np.ndarray | torch.Tensor):
+            The next state s_t+1 after taking action a_t from state s_t
 
-        done (Union[bool, torch.Tensor]):
-            Signalizes whether the end state is reached
+        done (np.ndarray | torch.Tensor):
+            Signalizes whether the next state s_t+1 is a terminal state
 
-        log_prob(Union[float, torch.Tensor], optional):
-            The log probability p(a | s)
+        log_prob(np.ndarray | torch.Tensor, optional):
+            The log probability p(a_t | s_t)
 
-        value(Union[float, torch.Tensor], optional):
-            The state-value V(s)
+        value(np.ndarray | torch.Tensor, optional):
+            The state-value function V(s_t)
     """
     state: Union[np.ndarray, torch.Tensor]
-    action: Union[int, torch.Tensor]
-    reward: Union[float, torch.Tensor]
+    action: Union[np.ndarray, torch.Tensor]
+    reward: Union[np.ndarray, torch.Tensor]
     next_state: Union[np.ndarray, torch.Tensor]
-    done: Union[bool, torch.Tensor]
-    log_prob: Optional[Union[float, torch.Tensor]] = None
-    value: Optional[Union[float, torch.Tensor]] = None
+    done: Union[np.ndarray, torch.Tensor]
+    log_prob: Optional[Union[np.ndarray, torch.Tensor]] = None
+    value: Optional[Union[np.ndarray, torch.Tensor]] = None
+
+    @staticmethod
+    def create(transitions: list["Transition"]) -> "Transition":
+        """
+        Creates a single transition in batch format from a list of single transitions.
+
+        Args:
+            transitions (list[Transition]):
+                The list of single transitions
+
+        Returns:
+            Transition:
+                A single transition in batch format
+        """
+        # Convert list[Transition] to a single Transition with matrices
+        states = torch.tensor(np.stack([t.state for t in transitions], axis=0)).to(dtype=torch.float32)
+        actions = torch.tensor(np.stack([t.action for t in transitions], axis=0)).to(dtype=torch.int64)
+        rewards = torch.tensor(np.stack([t.reward for t in transitions], axis=0)).to(dtype=torch.float32)
+        next_states = torch.tensor(np.stack([t.next_state for t in transitions], axis=0)).to(dtype=torch.float32)
+        dones = torch.tensor(np.stack([t.done for t in transitions], axis=0)).to(dtype=torch.bool)
+
+        log_probs = None
+        if transitions[0].log_prob is not None:
+            log_probs = torch.tensor(np.stack([t.log_prob for t in transitions], axis=0)).to(dtype=torch.float32)
+
+        values = None
+        if transitions[0].value is not None:
+            values = torch.tensor(np.stack([t.value for t in transitions], axis=0)).to(dtype=torch.float32)
+
+        return Transition(states, actions, rewards, next_states, dones, log_probs, values)
 
 
 class Buffer(ABC):
@@ -100,7 +130,7 @@ class Buffer(ABC):
     @abstractmethod
     def _push(self, transition: Transition):
         """
-        Pushes a Transition (s, a, r, s', done, log_prob, value) to the replay buffer.
+        Pushes a Transition (s_t, a_t, r_t, s_t+1, done, log_prob, value) to the replay buffer.
 
         Args:
             transition (Transition):
@@ -110,38 +140,38 @@ class Buffer(ABC):
 
     def push(
             self,
-            state: np.ndarray,
-            action: int,
-            reward: float,
-            next_state: np.ndarray,
-            done: bool,
-            log_prob: Optional[Union[float, torch.Tensor]] = None,
-            value: Optional[Union[float, torch.Tensor]] = None,
+            state: Union[np.ndarray, torch.Tensor],
+            action: Union[np.ndarray, torch.Tensor],
+            reward: Union[np.ndarray, torch.Tensor],
+            next_state: Union[np.ndarray, torch.Tensor],
+            done: Union[np.ndarray, torch.Tensor],
+            log_prob: Optional[Union[np.ndarray, torch.Tensor]] = None,
+            value: Optional[Union[np.ndarray, torch.Tensor]] = None,
     ):
         """
-        Pushes a Transition (s, a, r, s', done, log_prob, value) to the replay buffer.
+        Pushes a Transition (s_t, a_t, r_t, s_t+1, done, log_prob, value) to the replay buffer.
 
         Args:
-            state (np.ndarray):
-                The current state s
+            state (np.ndarray | torch.Tensor):
+                The current state s_t
 
-            action (int):
-                The taken action a
+            action (np.ndarray | torch.Tensor):
+                The taken action a_t
 
-            reward (float):
-                The reward r for taken action a from state s
+            reward (np.ndarray | torch.Tensor):
+                The reward r_t for taken action a_t from state s_t
 
-            next_state (np.ndarray):
-                The next state s' for taken action a from state s
+            next_state (np.ndarray | torch.Tensor):
+                The next state s_t+1 for taken action a_t from state s_t
 
-            done (bool):
-                Signalizes whether the end state is reached
+            done (np.ndarray | torch.Tensor):
+                Signalizes whether the next state s_t+1 is reached
             
-            log_prob (Union[float, torch.Tensor], optional):
-                The log probability of p(a | s)
+            log_prob (np.ndarray | torch.Tensor, optional):
+                The log probability of p(a_t | s_t)
 
-            value (Union[float, torch.Tensor], optional):
-                The state-value V(s)
+            value (np.ndarray | torch.Tensor, optional):
+                The state-value V(s_t)
         """
         return self._push(Transition(state, action, reward, next_state, done, log_prob, value))
 
@@ -173,23 +203,7 @@ class Buffer(ABC):
                 The transition as batch of PyTorch Tensors
         """
         samples = self._get(batch_size)
-
-        # Convert list[Transition] to a single Transition with matrices
-        states = torch.tensor(np.stack([t.state for t in samples], axis=1)).to(dtype=torch.float32)
-        actions = torch.tensor(np.stack([t.action for t in samples], axis=1)).to(dtype=torch.int64)
-        rewards = torch.tensor(np.stack([t.reward for t in samples], axis=1)).to(dtype=torch.float32)
-        next_states = torch.tensor(np.stack([t.next_state for t in samples], axis=1)).to(dtype=torch.float32)
-        dones = torch.tensor(np.stack([t.done for t in samples], axis=1)).to(dtype=torch.bool)
-
-        log_probs = None
-        if samples[0].log_prob is not None:
-            log_probs = torch.tensor(np.stack([t.log_prob for t in samples], axis=1)).to(dtype=torch.float32)
-
-        values = None
-        if samples[0].value is not None:
-            values = torch.tensor(np.stack([t.value for t in samples], axis=1)).to(dtype=torch.float32)
-
-        return Transition(states, actions, rewards, next_states, dones, log_probs, values)
+        return Transition.create(samples)
 
     @abstractmethod
     def _sample(self, batch_size: int) -> list[Transition]:
@@ -219,23 +233,7 @@ class Buffer(ABC):
                 The sampled transition as batches of PyTorch Tensors
         """
         samples = self._sample(batch_size)
-
-        # Convert list[Transition] to a single Transition with matrices
-        states = torch.tensor(np.array([t.state for t in samples])).to(dtype=torch.float32)
-        actions = torch.tensor(np.array([t.action for t in samples])).to(dtype=torch.int64)
-        rewards = torch.tensor(np.array([t.reward for t in samples])).to(dtype=torch.float32)
-        next_states = torch.tensor(np.array([t.next_state for t in samples])).to(dtype=torch.float32)
-        dones = torch.tensor(np.array([t.done for t in samples])).to(dtype=torch.bool)
-
-        log_probs = None
-        if samples[0].log_prob is not None:
-            log_probs = torch.tensor([t.log_prob for t in samples]).to(dtype=torch.float32)
-
-        values = None
-        if samples[0].value is not None:
-            values = torch.tensor([t.value for t in samples]).to(dtype=torch.float32)
-
-        return Transition(states, actions, rewards, next_states, dones, log_probs, values)
+        return Transition.create(samples)
 
     @abstractmethod
     def __len__(self) -> int:

PyRLAgent/common/buffer/ring_buffer.py
@@ -9,7 +9,7 @@ class RingBuffer(Buffer):
     """
     This class represents a circular replay buffer.
 
-    The circular replay buffer stores a specified maximum number of Transitions (s, a, r, s', done).
+    The circular replay buffer stores a specified maximum number of Transitions (s_t, a_t, r_t, s_t+1, done).
 
     If the maximum number of Transitions are reached (~ full), adding new Transitions will overwrite the oldest
     Transition in a circular manner after the LIFO (Last In, First Out) principle.
@@ -21,10 +21,8 @@ class RingBuffer(Buffer):
 
     def __init__(self, max_size: int):
         if max_size <= 0:
-            raise ValueError(
-                "Illegal max_size. "
-                "The argument should be >= 1!"
-            )
+            raise ValueError("Illegal max_size. The argument should be >= 1!")
+
         self.max_size = max_size
         self.memory = deque(maxlen=max_size)
 
@@ -36,46 +34,27 @@ class RingBuffer(Buffer):
 
     def filled(self, min_size: int) -> bool:
         if min_size <= 0 or min_size > self.max_size:
-            raise ValueError(
-                "Illegal min_size. "
-                "The argument should be 1 <= min_size <= max_size!"
-            )
-
+            raise ValueError("Illegal min_size. The argument should be 1 <= min_size <= max_size!")
         return len(self) >= min_size
 
     def _push(self, transition: Transition):
         if self.full():
-            # Case: Drop the oldest transition from the memory
             self.memory.popleft()
-
-        # Append a single transition to the memory
         self.memory.append(transition)
 
     def _get(self, batch_size: int) -> list[Transition]:
         if batch_size <= 0:
-            raise ValueError(
-                "Illegal batch_size. "
-                "The argument should be >= 1!"
-            )
+            raise ValueError("Illegal batch_size. The argument should be >= 1!")
         if not self.filled(batch_size):
-            raise ValueError(
-                "Illegal call of get()!"
-                "There are not enough transitions stored to return from the buffer!"
-            )
+            raise ValueError("Illegal call of get()! There are not enough transitions stored!")
 
         return [self.memory[idx] for idx in range(batch_size)]
 
     def _sample(self, batch_size: int) -> list[Transition]:
         if batch_size <= 0:
-            raise ValueError(
-                "Illegal batch_size. "
-                "The argument should be >= 1!"
-            )
+            raise ValueError("Illegal batch_size. The argument should be >= 1!")
         if not self.filled(batch_size):
-            raise ValueError(
-                "Illegal call of sample()!"
-                "There are not enough transitions stored to sample from the buffer!"
-            )
+            raise ValueError("Illegal call of sample()! There are not enough transitions stored!")
 
         indices = torch.randint(0, len(self), size=(batch_size,))
         return [self.memory[idx] for idx in indices]
@@ -96,13 +75,11 @@ class RingBuffer(Buffer):
         raise NotImplementedError
 
     def __getstate__(self) -> dict:
-        """ Magic function to save a custom class as yaml file. """
         return {
             "max_size": self.max_size,
             "memory": self.memory,
         }
 
     def __setstate__(self, state: dict):
-        """ Magic function to load a custom class from yaml file. """
         self.max_size = state["max_size"]
         self.memory = state["memory"]

tests/common/buffer/test_ring_buffer.py
@@ -15,6 +15,13 @@ class TestRingBuffer(unittest.TestCase):
     def setUp(self):
         self.max_size = 5
         self.replay_buffer = RingBuffer(max_size=self.max_size)
+        self.state = np.array([1, 2, 3, 4, 5])
+        self.action = np.array([0])
+        self.reward = np.array([1])
+        self.next_state = np.array([2, 3, 4, 5, 6])
+        self.done = np.array([True])
+        self.log_prob = np.array([0.23456])
+        self.value = np.array([0])
 
     def test_reset(self):
         """
@@ -22,7 +29,9 @@ class TestRingBuffer(unittest.TestCase):
         """
         # Fill values
         for i in range(self.max_size):
-            self.replay_buffer.push(np.array([i]), i, i, np.array([i + 1]), False)
+            self.replay_buffer.push(
+                self.state, self.action, self.reward, self.next_state, self.done, self.log_prob, self.value
+            )
         self.replay_buffer.reset()
 
         # Check if no transition is in the ring buffer
@@ -37,7 +46,9 @@ class TestRingBuffer(unittest.TestCase):
 
         # Fill values
         for i in range(self.max_size):
-            self.replay_buffer.push(np.array([i]), i, i, np.array([i + 1]), False)
+            self.replay_buffer.push(
+                self.state, self.action, self.reward, self.next_state, self.done, self.log_prob, self.value
+            )
 
         # Check if the ring buffer is full (full=True)
         self.assertTrue(self.replay_buffer.full())
@@ -49,7 +60,9 @@ class TestRingBuffer(unittest.TestCase):
         # Fill 3 values
         size = self.max_size - 2
         for i in range(size):
-            self.replay_buffer.push(np.array([i]), i, i, np.array([i + 1]), False)
+            self.replay_buffer.push(
+                self.state, self.action, self.reward, self.next_state, self.done, self.log_prob, self.value
+            )
 
         # Check if ring buffer is filled with 3 (filled=True) and 5 values (filled=False)
         self.assertTrue(self.replay_buffer.filled(size))
@@ -59,7 +72,9 @@ class TestRingBuffer(unittest.TestCase):
         """
         Tests the method push().
         """
-        self.replay_buffer.push(np.array([0]), 0, 0, np.array([0 + 1]), False)
+        self.replay_buffer.push(
+            self.state, self.action, self.reward, self.next_state, self.done, self.log_prob, self.value
+        )
 
         self.assertEqual(1, len(self.replay_buffer))
 
@@ -69,7 +84,9 @@ class TestRingBuffer(unittest.TestCase):
         """
         size = self.max_size - 2
         for i in range(self.max_size):
-            self.replay_buffer.push(np.array([i]), np.array([i]), np.array([i]), np.array([i + 1]), np.array([False]))
+            self.replay_buffer.push(
+                self.state, self.action, self.reward, self.next_state, self.done, self.log_prob, self.value
+            )
 
         samples = self.replay_buffer.get(batch_size=size)
 
@@ -79,11 +96,11 @@ class TestRingBuffer(unittest.TestCase):
         self.assertIsInstance(samples.next_state, torch.Tensor)
         self.assertIsInstance(samples.done, torch.Tensor)
 
-        self.assertEqual((1, size), samples.state.shape)
-        self.assertEqual((1, size), samples.action.shape)
-        self.assertEqual((1, size), samples.reward.shape)
-        self.assertEqual((1, size), samples.next_state.shape)
-        self.assertEqual((1, size), samples.done.shape)
+        self.assertEqual((size, 5), samples.state.shape)
+        self.assertEqual((size, 1), samples.action.shape)
+        self.assertEqual((size, 1), samples.reward.shape)
+        self.assertEqual((size, 5), samples.next_state.shape)
+        self.assertEqual((size, 1), samples.done.shape)
 
     def test_sample(self):
         """
@@ -91,7 +108,9 @@ class TestRingBuffer(unittest.TestCase):
         """
         size = self.max_size - 2
         for i in range(self.max_size):
-            self.replay_buffer.push(np.array([i]), i, i, np.array([i + 1]), False)
+            self.replay_buffer.push(
+                self.state, self.action, self.reward, self.next_state, self.done, self.log_prob, self.value
+            )
 
         samples = self.replay_buffer.sample(batch_size=size)
 
@@ -101,11 +120,11 @@ class TestRingBuffer(unittest.TestCase):
         self.assertIsInstance(samples.next_state, torch.Tensor)
         self.assertIsInstance(samples.done, torch.Tensor)
 
-        self.assertEqual(size, len(samples.state))
-        self.assertEqual(size, len(samples.action))
-        self.assertEqual(size, len(samples.reward))
-        self.assertEqual(size, len(samples.next_state))
-        self.assertEqual(size, len(samples.done))
+        self.assertEqual((size, 5), samples.state.shape)
+        self.assertEqual((size, 1), samples.action.shape)
+        self.assertEqual((size, 1), samples.reward.shape)
+        self.assertEqual((size, 5), samples.next_state.shape)
+        self.assertEqual((size, 1), samples.done.shape)
 
     def test_len(self):
         """
@@ -115,7 +134,9 @@ class TestRingBuffer(unittest.TestCase):
 
         # Fill values
         for i in range(self.max_size):
-            self.replay_buffer.push(np.array([i]), i, i, np.array([i + 1]), False)
+            self.replay_buffer.push(
+                self.state, self.action, self.reward, self.next_state, self.done, self.log_prob, self.value
+            )
 
         self.assertEqual(self.max_size, len(self.replay_buffer))
 
@@ -124,8 +145,12 @@ class TestRingBuffer(unittest.TestCase):
         Tests the magic functions __getstate__ and __setstate__.
         """
         # Let push some values to the replay buffer
-        self.replay_buffer.push(np.array([0]), 0, 0, np.array([1]), False)
-        self.replay_buffer.push(np.array([1]), 1, 1, np.array([2]), False)
+        self.replay_buffer.push(
+            self.state, self.action, self.reward, self.next_state, self.done, self.log_prob, self.value
+        )
+        self.replay_buffer.push(
+            self.state, self.action, self.reward, self.next_state, self.done, self.log_prob, self.value
+        )
 
         # Safe the hyperparameter as yaml file
         with open("test_data.yaml", "w") as yaml_file:
@@ -136,7 +161,28 @@ class TestRingBuffer(unittest.TestCase):
             replay_buffer = yaml.load(yaml_file, Loader=yaml.Loader)
 
         # Check if they are equal
-        self.assertEqual(replay_buffer, self.replay_buffer)
+        self.assertEqual(self.replay_buffer.max_size, replay_buffer.max_size)
+        np.testing.assert_array_almost_equal(self.replay_buffer.memory[0].state, replay_buffer.memory[0].state)
+        np.testing.assert_array_almost_equal(self.replay_buffer.memory[0].action, replay_buffer.memory[0].action)
+        np.testing.assert_array_almost_equal(self.replay_buffer.memory[0].reward, replay_buffer.memory[0].reward)
+        np.testing.assert_array_almost_equal(
+            self.replay_buffer.memory[0].next_state,
+            replay_buffer.memory[0].next_state
+        )
+        np.testing.assert_array_almost_equal(self.replay_buffer.memory[0].done, replay_buffer.memory[0].done)
+        np.testing.assert_array_almost_equal(self.replay_buffer.memory[0].log_prob, replay_buffer.memory[0].log_prob)
+        np.testing.assert_array_almost_equal(self.replay_buffer.memory[0].value, replay_buffer.memory[0].value)
+
+        np.testing.assert_array_almost_equal(self.replay_buffer.memory[1].state, replay_buffer.memory[1].state)
+        np.testing.assert_array_almost_equal(self.replay_buffer.memory[1].action, replay_buffer.memory[1].action)
+        np.testing.assert_array_almost_equal(self.replay_buffer.memory[1].reward, replay_buffer.memory[1].reward)
+        np.testing.assert_array_almost_equal(
+            self.replay_buffer.memory[1].next_state,
+            replay_buffer.memory[1].next_state
+        )
+        np.testing.assert_array_almost_equal(self.replay_buffer.memory[1].done, replay_buffer.memory[1].done)
+        np.testing.assert_array_almost_equal(self.replay_buffer.memory[1].log_prob, replay_buffer.memory[1].log_prob)
+        np.testing.assert_array_almost_equal(self.replay_buffer.memory[1].value, replay_buffer.memory[1].value)
 
 
 if __name__ == '__main__':